89146c4bea148bac0ada9b8a02016a70a6f4a5ea
kedar pathak <kedar.pathak@pragtech.co.in>
2014-01-06 16:24:51 +0530

    smtp configuration is added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Quickbook42::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+   config.action_mailer.default_url_options = { :host => '178.63.19.197' }
+   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address        =>  "smtp.gmail.com",
+    :port           =>  587,
+    :domain         =>  "gmail.com",
+    :authentication =>  :login ,
+    :user_name      =>  "demo.pragmatic@gmail.com",
+    :password       =>  "pragmatic"
+    
+  } 
 end
