6476437dd81a3fadb31b3a4ba46663b1f644d6cb
kedar pathak <kedar.pathak@pragtech.co.in>
2014-02-10 11:24:21 +0530

    changed a steps flow

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -171,11 +171,18 @@ private
 #        )
 #  end
 
-  def gateway
+ # def gateway
+ #       @gateway ||= PaypalExpressGateway.new(
+ #          :login => 'kedar.pathak-facilitator_api1.pragtech.co.in',
+ #         :password => '1364994877',
+ #         :signature => 'ACLa8jsQN8TPFLDY57dLNb5-3qq.AgN5u20e33t3nrXP3uDzoZTGNERk'
+ #       )
+ # end
+   def gateway
         @gateway ||= PaypalExpressGateway.new(
-           :login => 'kedar.pathak-facilitator_api1.pragtech.co.in',
-          :password => '1364994877',
-          :signature => 'ACLa8jsQN8TPFLDY57dLNb5-3qq.AgN5u20e33t3nrXP3uDzoZTGNERk'
+           :login => 'paypal_api1.pragtech.co.in',
+          :password => 'V996T2LW3QBLPZFB',
+          :signature => 'AFcWxV21C7fd0v3bYYYRCpSSRl31ATdQOOCe1m3eBpjpnp7dZ6rE22q5'
         )
   end
   
